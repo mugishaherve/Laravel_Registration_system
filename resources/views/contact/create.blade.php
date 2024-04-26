064852220641130a2f136e9911289a2d4af118a3
herve <mugishaherve1000@gmail.com>
2024-04-26 08:16:47 +0200

    creating routes, and updating the models and controllers

---
+++ resources/views/contact/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+@section('content')
+<div class="card">
+
+    <div class="card-header">Register form</div>
+    <div class="card-body">
+        <form action="{{route('Register) }}" method="post">
+
+            {!! csrf_field() !!}
+            <h2>Registration Form</h2>
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" class="form-control">
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" class="form-control">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" class="form-control">
+            <input type="submit" value="save" class="btn btn-success">
+        </form>
+    </div>
+</div>
+
+
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+
+    @yield('content')
+
+        <div class="card bg-secondary text-white">
+            <div class="card-body">Copyright (c) 2024 Herve</div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
